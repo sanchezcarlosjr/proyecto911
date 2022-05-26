983e422a1f9425b14b028d3287ca6aec2fccd31e
moonslayers <jorge.marquez.luna@uabc.edu.mx>
2022-05-25 20:08:29 -0700

    form insert bug fixed

--- server.js
+++ server.js
@@ -223,6 +223,29 @@ app.post('/convenios',encoder,function(req,res){
   res.redirect('/main');
 });
 
+app.post('/convenios',encoder,function(req,res){
+  var con = require('./config');
+  if(req.body.form=="guardar"){
+    var sql = "UPDATE convenios SET PERIODO_ID=?,PERIODO=?,CONVENIO_VINCID=?,CONVENIO_VINC=?,FECHA=?,SECTOR_ID=?,SECTOR=?"
+    +",ORIGEN_ID=?,ORIGEN=?,PAIS_VINC=?,INST_ORG=?,COOP=?,INVE=?,INTER=?,MOVI=?,validar=? WHERE ID="+req.body.id;
+    
+      con.query(sql, [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.convenioVincId),
+        req.body.convenioVinc,req.body.fecha,parseInt(req.body.sectorId),
+        req.body.sector,parseInt(req.body.origenId),req.body.origen,
+        req.body.paisVinc,req.body.instOrg,parseInt(req.body.coop),
+        parseInt(req.body.inve),parseInt(req.body.inter),parseInt(req.body.movi),1,parseInt(req.body.id)], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records updated: " + result.affectedRows);
+      });
+  }else if(req.body.form=="validar"){
+    con.query("update convenios set validar=1 where ID="+req.body.id,function(err,row){
+      if(err) throw err;
+      console.log("Number of record validated: " + row.affectedRows);
+    });
+  }
+  res.redirect('/main');
+});
+
 //formularios inputs
 app.post('/', encoder, function(req, res){
   var con = require('./config');
@@ -252,194 +275,182 @@ app.post('/', encoder, function(req, res){
   }
   
   if(req.body.form == "movilidad-entrada"){
-    con.connect(function(err) {
+    console.log("Connected!");
+    var sql = "INSERT INTO movilidad_academica_entrada (`PERIODO_ID`,`PERIODO`,`CAMPUS_ID`,`CAMPUS_DESC`,`UNIDAD_ID`,`UNIDAD`,"
+    +"`VISITANTE_ID`,`VISITANTE_NOMBRE`,`VISITANTE_APELLIDO1`,`VISITANTE_APELLIDO2`,`SEXO_ID`,`SEXO`,`NIVEL_ID`,`NIVEL`,`DISCAPACIDAD`,"
+    +"`HABLANTE_INDIGENA`,`ORIGEN_INDIGENA`,`UE`,`UE_PAIS`,`UE_ENTIDAD`,`UE_IDIOMA`,`TMA_ID`,`TMA`,`validar`,`AUTOR`} ) VALUES ?";
+    var sexo,nivel,tma;
+    
+    if(req.body.sexoId == 1)
+      sexo="Femenino"
+    else
+      sexo="Masculino"
+    if(req.body.nivelId == 1)
+      nivel="Licenciatura"
+    else if (req.body.nivelId == 2)
+      nivel="Especialidad"
+    else if (req.body.nivelId == 3)
+      nivel="Maestria"
+    else
+      nivel="Doctorado"
+    
+    if(req.body.tmaId == 1)
+      tma="Docencia"
+    else if(req.body.tmaId ==2)
+      tma="Estancias Sabaticas"
+    else
+      tma="Estancia de Investigacion"
+    
+    
+    var values = [
+      [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.campusId),
+      req.body.campus,parseInt(req.body.unidadId),req.body.unidad,
+      parseInt(req.body.visistanteId),req.body.visitante_nombre,
+      req.body.apellido1,req.body.apellido2,parseInt(req.body.sexoId),
+      sexo,parseInt(req.body.nivelId),nivel,parseInt(req.body.discapacidadId),
+      parseInt(req.body.hablanteId),parseInt(req.body.origenId),req.body.ue,
+      req.body.uePais,req.body.ueEntidad,req.body.ueIdioma,
+      parseInt(req.body.tmaId),tma,0,req.session.username]
+    ];
+    con.query(sql, [values], function (err, result) {
       if (err) throw err;
-      console.log("Connected!");
-      var sql = "INSERT INTO movilidad_academica_entrada (`PERIODO_ID`,`PERIODO`,`CAMPUS_ID`,`CAMPUS_DESC`,`UNIDAD_ID`,`UNIDAD`,"
-      +"`VISITANTE_ID`,`VISITANTE_NOMBRE`,`VISITANTE_APELLIDO1`,`VISITANTE_APELLIDO2`,`SEXO_ID`,`SEXO`,`NIVEL_ID`,`NIVEL`,`DISCAPACIDAD`,"
-      +"`HABLANTE_INDIGENA`,`ORIGEN_INDIGENA`,`UE`,`UE_PAIS`,`UE_ENTIDAD`,`UE_IDIOMA`,`TMA_ID`,`TMA`,`validar`,`AUTOR`} ) VALUES ?";
-      var sexo,nivel,tma;
-      
-      if(req.body.sexoId == 1)
-        sexo="Femenino"
-      else
-        sexo="Masculino"
-      if(req.body.nivelId == 1)
-        nivel="Licenciatura"
-      else if (req.body.nivelId == 2)
-        nivel="Especialidad"
-      else if (req.body.nivelId == 3)
-        nivel="Maestria"
-      else
-        nivel="Doctorado"
-      
-      if(req.body.tmaId == 1)
-        tma="Docencia"
-      else if(req.body.tmaId ==2)
-        tma="Estancias Sabaticas"
-      else
-        tma="Estancia de Investigacion"
-      
-      
-      var values = [
-        [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.campusId),
-        req.body.campus,parseInt(req.body.unidadId),req.body.unidad,
-        parseInt(req.body.visistanteId),req.body.visitante_nombre,
-        req.body.apellido1,req.body.apellido2,parseInt(req.body.sexoId),
-        sexo,parseInt(req.body.nivelId),nivel,parseInt(req.body.discapacidadId),
-        parseInt(req.body.hablanteId),parseInt(req.body.origenId),req.body.ue,
-        req.body.uePais,req.body.ueEntidad,req.body.ueIdioma,
-        parseInt(req.body.tmaId),tma,0,req.session.username]
-      ];
-      con.query(sql, [values], function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
-      });
+      console.log("Number of records inserted: " + result.affectedRows);
     });
     console.log(req.body.periodo);
     res.redirect('/main');
   }else if(req.body.form == "movilidad-salida"){
-      console.log("Connected!");
-      var sql = "INSERT INTO movilidad_academica_salida (`PERIODO_ID`,`PERIODO`,`CAMPUS_ID`,`CAMPUS_DESC`,`UNIDAD_ID`,`UNIDAD`,"
-      +"`EMPLEADO_ID`,`EMPLEADO_NOMBRE`,`EMPLEADO_APELLIDO1`,`EMPLEADO_APELLIDO2`,`SEXO_ID`,`SEXO`,"
-      +"`UR`,`UR_PAIS`,`UR_ENTIDAD`,`UR_IDIOMA`,`TMA_ID`,`TMA`,`validar`,`AUTOR`) VALUES ?";
-      var tma,sexo,nivel;
-      
-      if(req.body.sexoId == 1)
-        sexo="Femenino"
-      else
-        sexo="Masculino"
-      if(req.body.nivelId == 1)
-        nivel="Licenciatura"
-      else if (req.body.nivelId == 2)
-        nivel="Especialidad"
-      else if (req.body.nivelId == 3)
-        nivel="Maestria"
-      else
-        nivel="Doctorado"
-      
-      if(req.body.tmaId == 1)
-        tma="Docencia"
-      else if(req.body.tmaId ==2)
-        tma="Estancias Sabaticas"
-      else
-        tma="Estancia de Investigacion"
-      
-      var values = [
-        [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.campusId),
-        req.body.campus,parseInt(req.body.unidadId),req.body.unidad,
-        parseInt(req.body.empleadoId),req.body.empleado_nombre,
-        req.body.apellido1,req.body.apellido2,parseInt(req.body.sexoId),
-        sexo,req.body.ur,req.body.urPais,req.body.urEntidad,
-        req.body.urIdioma,parseInt(req.body.tmaId),tma,0,req.session.username]
-      ];
-      con.query(sql, [values], function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
-      });
-    console.log(req.body.form);
-    res.redirect('/main');
-  }else if(req.body.form == "convenios"){
-    con.connect(function(err) {
+    console.log("Connected!");
+    var sql = "INSERT INTO movilidad_academica_salida (`PERIODO_ID`,`PERIODO`,`CAMPUS_ID`,`CAMPUS_DESC`,`UNIDAD_ID`,`UNIDAD`,"
+    +"`EMPLEADO_ID`,`EMPLEADO_NOMBRE`,`EMPLEADO_APELLIDO1`,`EMPLEADO_APELLIDO2`,`SEXO_ID`,`SEXO`,"
+    +"`UR`,`UR_PAIS`,`UR_ENTIDAD`,`UR_IDIOMA`,`TMA_ID`,`TMA`,`validar`,`AUTOR`) VALUES ?";
+    var tma,sexo,nivel;
+    
+    if(req.body.sexoId == 1)
+      sexo="Femenino"
+    else
+      sexo="Masculino"
+    if(req.body.nivelId == 1)
+      nivel="Licenciatura"
+    else if (req.body.nivelId == 2)
+      nivel="Especialidad"
+    else if (req.body.nivelId == 3)
+      nivel="Maestria"
+    else
+      nivel="Doctorado"
+    
+    if(req.body.tmaId == 1)
+      tma="Docencia"
+    else if(req.body.tmaId ==2)
+      tma="Estancias Sabaticas"
+    else
+      tma="Estancia de Investigacion"
+    
+    var values = [
+      [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.campusId),
+      req.body.campus,parseInt(req.body.unidadId),req.body.unidad,
+      parseInt(req.body.empleadoId),req.body.empleado_nombre,
+      req.body.apellido1,req.body.apellido2,parseInt(req.body.sexoId),
+      sexo,req.body.ur,req.body.urPais,req.body.urEntidad,
+      req.body.urIdioma,parseInt(req.body.tmaId),tma,0,req.session.username]
+    ];
+    con.query(sql, [values], function (err, result) {
       if (err) throw err;
-      console.log("Connected!");
-      var sql = "INSERT INTO convenios (`PERIODO_ID`,`PERIODO`,`CONVENIO_VINCID`,`CONVENIO_VINC`,`FECHA`,`SECTOR_ID`,"
-      +"`SECTOR`,`ORIGEN_ID`,`ORIGEN`,`PAIS_VINC`,`INST_ORG`,"
-      +"`COOP`,`INVE`,`INTER`,`MOVI`,`validar`,`AUTOR`) VALUES ?";
-      
-      var values = [
-        [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.convenioVincId),
-        req.body.convenioVinc,req.body.fecha,parseInt(req.body.sectorId),
-        req.body.sector,parseInt(req.body.origenId),req.body.origen,
-        req.body.paisVinc,req.body.instOrg,parseInt(req.body.coop),
-        parseInt(req.body.inve),parseInt(req.body.inter),parseInt(req.body.movi),0,req.session.username]
-      ];
-      con.query(sql, [values], function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
-      });
+      console.log("Number of records inserted: " + result.affectedRows);
+    });
+  console.log(req.body.form);
+  res.redirect('/main');
+}else if(req.body.form == "convenios"){
+    console.log("Connected!");
+    var sql = "INSERT INTO convenios (`PERIODO_ID`,`PERIODO`,`CONVENIO_VINCID`,`CONVENIO_VINC`,`FECHA`,`SECTOR_ID`,"
+    +"`SECTOR`,`ORIGEN_ID`,`ORIGEN`,`PAIS_VINC`,`INST_ORG`,"
+    +"`COOP`,`INVE`,`INTER`,`MOVI`,`validar`,`AUTOR`) VALUES ?";
+    
+    var values = [
+      [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.convenioVincId),
+      req.body.convenioVinc,req.body.fecha,parseInt(req.body.sectorId),
+      req.body.sector,parseInt(req.body.origenId),req.body.origen,
+      req.body.paisVinc,req.body.instOrg,parseInt(req.body.coop),
+      parseInt(req.body.inve),parseInt(req.body.inter),parseInt(req.body.movi),0,req.session.username]
+    ];
+    con.query(sql, [values], function (err, result) {
+      if (err) throw err;
+      console.log("Number of records inserted: " + result.affectedRows);
     });
     console.log(req.body.form);
     res.redirect('/main');
   }else if(req.body.form == "intercambio-entrada"){
-    con.connect(function(err) {
+    console.log("Connected!");
+    var sql = "INSERT INTO intercambio_estudiantil_entrada (`PERIODO_ID`,`PERIODO`,`CAMPUS_ID`,`CAMPUS_DESC`,`UNIDAD_ID`,`UNIDAD`,"
+    +"`NIVEL_ID`,`NIVEL`,`PROGRAMA_ID`,`PROGRAMA_DESC`,`AREA_ID`,`AREA`,`ESTUDIANTE_ID`,`ESTUDIANTE_NOMBRE`,`ESTUDIANTE_APELLIDO1`,`ESTUDIANTE_APELLIDO2`,`SEXO_ID`,`SEXO`,`DISCAPACIDAD`,"
+    +"`HABLANTE_INDIGENA`,`ORIGEN_INDIGENA`,`UR`,`UR_PAIS`,`UR_ENTIDAD`,`UR_IDIOMA`,`FINAN_ID`,`FINAN`,`FINAN_VAL`,`DATE_START`,`DATE_END`,`validar`,`AUTOR`) VALUES ?"
+    var nivel,sexo,finan;
+    
+    if(req.body.sexoId == 1)
+      sexo="Femenino"
+    else
+      sexo="Masculino"
+      
+    if(req.body.finanId == 1)
+      finan="Si"
+    else
+      finan="No"
+    
+    if(req.body.nivelId == 1)
+      nivel="Licenciatura"
+    else if (req.body.nivelId == 2)
+      nivel="Especialidad"
+    else if (req.body.nivelId == 3)
+      nivel="Maestria"
+    else
+      nivel="Doctorado"
+    
+    var values = [
+      [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.campusId),req.body.campus,parseInt(req.body.unidadId),req.body.unidad,
+      parseInt(req.body.nivelId),nivel,parseInt(req.body.programaId),req.body.programa,parseInt(req.body.areaId),req.body.area,parseInt(req.body.estudianteId),
+      req.body.estudiante_nombre,req.body.apellido1,req.body.apellido2,parseInt(req.body.sexoId),sexo,parseInt(req.body.discapacidadId),parseInt(req.body.hablanteId),parseInt(req.body.origenId),
+      req.body.ue,req.body.uePais,req.body.ueEntidad,req.body.ueIdioma,parseInt(req.body.finanId),finan,parseInt(req.body.finan_val),req.body.fechaInicio,req.body.fechaFinal,0,req.session.username]
+    ];
+    con.query(sql, [values], function (err, result) {
       if (err) throw err;
-      console.log("Connected!");
-      var sql = "INSERT INTO intercambio_estudiantil_entrada (`PERIODO_ID`,`PERIODO`,`CAMPUS_ID`,`CAMPUS_DESC`,`UNIDAD_ID`,`UNIDAD`,"
-      +"`NIVEL_ID`,`NIVEL`,`PROGRAMA_ID`,`PROGRAMA_DESC`,`AREA_ID`,`AREA`,`ESTUDIANTE_ID`,`ESTUDIANTE_NOMBRE`,`ESTUDIANTE_APELLIDO1`,`ESTUDIANTE_APELLIDO2`,`SEXO_ID`,`SEXO`,`DISCAPACIDAD`,"
-      +"`HABLANTE_INDIGENA`,`ORIGEN_INDIGENA`,`UR`,`UR_PAIS`,`UR_ENTIDAD`,`UR_IDIOMA`,`FINAN_ID`,`FINAN`,`FINAN_VAL`,`DATE_START`,`DATE_END`,`validar`,`AUTOR`) VALUES ?"
-      var nivel,sexo,finan;
-      
-      if(req.body.sexoId == 1)
-        sexo="Femenino"
-      else
-        sexo="Masculino"
-        
-      if(req.body.finanId == 1)
-        finan="Si"
-      else
-        finan="No"
-      
-      if(req.body.nivelId == 1)
-        nivel="Licenciatura"
-      else if (req.body.nivelId == 2)
-        nivel="Especialidad"
-      else if (req.body.nivelId == 3)
-        nivel="Maestria"
-      else
-        nivel="Doctorado"
-      
-      var values = [
-        [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.campusId),req.body.campus,parseInt(req.body.unidadId),req.body.unidad,
-        parseInt(req.body.nivelId),nivel,parseInt(req.body.programaId),req.body.programa,parseInt(req.body.areaId),req.body.area,parseInt(req.body.estudianteId),
-        req.body.estudiante_nombre,req.body.apellido1,req.body.apellido2,parseInt(req.body.sexoId),sexo,parseInt(req.body.discapacidadId),parseInt(req.body.hablanteId),parseInt(req.body.origenId),
-        req.body.ue,req.body.uePais,req.body.ueEntidad,req.body.ueIdioma,parseInt(req.body.finanId),finan,parseInt(req.body.finan_val),req.body.fechaInicio,req.body.fechaFinal,0,req.session.username]
-      ];
-      con.query(sql, [values], function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
-      });
+      console.log("Number of records inserted: " + result.affectedRows);
     });
     console.log(req.body.form);
     res.redirect('/main');
   }else if(req.body.form == "intercambio-salida"){
-    con.connect(function(err) {
+    console.log("Connected!");
+    var sql = "INSERT INTO intercambio_estudiantil_salida (`PERIODO_ID`,`PERIODO`,`CAMPUS_ID`,`CAMPUS_DESC`,`UNIDAD_ID`,`UNIDAD`,"
+    +"`NIVEL_ID`,`NIVEL`,`PROGRAMA_ID`,`PROGRAMA_DESC`,`AREA_ID`,`AREA`,`ESTUDIANTE_ID`,`ESTUDIANTE_NOMBRE`,`ESTUDIANTE_APELLIDO1`,`ESTUDIANTE_APELLIDO2`,`SEXO_ID`,`SEXO`,`DISCAPACIDAD`,"
+    +"`HABLANTE_INDIGENA`,`ORIGEN_INDIGENA`,`UR`,`UR_PAIS`,`UR_ENTIDAD`,`UR_IDIOMA`,`FINAN_ID`,`FINAN`,`FINAN_VAL`,`DATE_START`,`DATE_END`,`validar`,`AUTOR`) VALUES ?"
+    var nivel,sexo,finan;
+    
+    if(req.body.sexoId == 1)
+      sexo="Femenino"
+    else
+      sexo="Masculino"
+      
+    if(req.body.finanId == 1)
+      finan="Si"
+    else
+      finan="No"
+    
+    if(req.body.nivelId == 1)
+      nivel="Licenciatura"
+    else if (req.body.nivelId == 2)
+      nivel="Especialidad"
+    else if (req.body.nivelId == 3)
+      nivel="Maestria"
+    else
+      nivel="Doctorado"
+    
+    var values = [
+      [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.campusId),req.body.campus,parseInt(req.body.unidadId),req.body.unidad,
+      parseInt(req.body.nivelId),nivel,parseInt(req.body.programaId),req.body.programa,parseInt(req.body.areaId),req.body.area,parseInt(req.body.estudianteId),
+      req.body.estudiante_nombre,req.body.apellido1,req.body.apellido2,parseInt(req.body.sexoId),sexo,parseInt(req.body.discapacidadId),parseInt(req.body.hablanteId),parseInt(req.body.origenId),
+      req.body.ue,req.body.uePais,req.body.ueEntidad,req.body.ueIdioma,parseInt(req.body.finanId),finan,parseInt(req.body.finan_val),req.body.fechaInicio,req.body.fechaFinal,0,req.session.username]
+    ];
+    con.query(sql, [values], function (err, result) {
       if (err) throw err;
-      console.log("Connected!");
-      var sql = "INSERT INTO intercambio_estudiantil_salida (`PERIODO_ID`,`PERIODO`,`CAMPUS_ID`,`CAMPUS_DESC`,`UNIDAD_ID`,`UNIDAD`,"
-      +"`NIVEL_ID`,`NIVEL`,`PROGRAMA_ID`,`PROGRAMA_DESC`,`AREA_ID`,`AREA`,`ESTUDIANTE_ID`,`ESTUDIANTE_NOMBRE`,`ESTUDIANTE_APELLIDO1`,`ESTUDIANTE_APELLIDO2`,`SEXO_ID`,`SEXO`,`DISCAPACIDAD`,"
-      +"`HABLANTE_INDIGENA`,`ORIGEN_INDIGENA`,`UR`,`UR_PAIS`,`UR_ENTIDAD`,`UR_IDIOMA`,`FINAN_ID`,`FINAN`,`FINAN_VAL`,`DATE_START`,`DATE_END`,`validar`,`AUTOR`) VALUES ?"
-      var nivel,sexo,finan;
-      
-      if(req.body.sexoId == 1)
-        sexo="Femenino"
-      else
-        sexo="Masculino"
-        
-      if(req.body.finanId == 1)
-        finan="Si"
-      else
-        finan="No"
-      
-      if(req.body.nivelId == 1)
-        nivel="Licenciatura"
-      else if (req.body.nivelId == 2)
-        nivel="Especialidad"
-      else if (req.body.nivelId == 3)
-        nivel="Maestria"
-      else
-        nivel="Doctorado"
-      
-      var values = [
-        [parseInt(req.body.periodoId),req.body.periodo,parseInt(req.body.campusId),req.body.campus,parseInt(req.body.unidadId),req.body.unidad,
-        parseInt(req.body.nivelId),nivel,parseInt(req.body.programaId),req.body.programa,parseInt(req.body.areaId),req.body.area,parseInt(req.body.estudianteId),
-        req.body.estudiante_nombre,req.body.apellido1,req.body.apellido2,parseInt(req.body.sexoId),sexo,parseInt(req.body.discapacidadId),parseInt(req.body.hablanteId),parseInt(req.body.origenId),
-        req.body.ue,req.body.uePais,req.body.ueEntidad,req.body.ueIdioma,parseInt(req.body.finanId),finan,parseInt(req.body.finan_val),req.body.fechaInicio,req.body.fechaFinal,0,req.session.username]
-      ];
-      con.query(sql, [values], function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
-      });
+      console.log("Number of records inserted: " + result.affectedRows);
     });
     console.log(req.body.form);
     res.redirect('/main');
