f1d9c02534878b8cd63c8592062d1e1fbc4ea512
Andres Herrera <18634968+otherandy@users.noreply.github.com>
2022-10-24 01:13:10 -0700

    feat(session): user auth from api

--- controllers/session.ts
+++ controllers/session.ts
@@ -1,6 +1,5 @@
 import User from "../models/user";
-const hasher = require("pbkdf2-password")();
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const sign = (body: Object) => {
   return new Promise((resolve, reject) => {
@@ -8,23 +7,30 @@ const sign = (body: Object) => {
       body,
       process.env.JWT_SECRET || "secret",
       {
-        expiresIn: '1h'
+        expiresIn: "1h",
       },
       (err, token) => {
-         resolve(token); 
+        resolve(token);
       }
     );
   });
-}
+};
 
 export default {
   getToken: async (email: string, password: string) => {
-      const user = await (User as any).findByEmail(email) as any;
-      return await hasher(password, async (err: any, pass: any, salt: any, hash: any) => {
-        if (hash !== user.hash) {
-          throw new Error("invalid password");
-        }
-        return await sign({email});
-    }) as string;
-  }
+    const formData = new URLSearchParams() as any;
+    formData.append("json", JSON.stringify({ usuario: process.env.API_USERNAME, contrasena: process.env.API_PASSWORD }));
+    const res = await fetch(`${process.env.API_URL}/escolar/alumno/auth`, {
+      method: "POST",
+      headers: {
+        "Authorization": `Basic ${Buffer.from(`${process.env.API_USERNAME}:${process.env.API_PASSWORD}`).toString("base64")}`,
+      },
+      body: formData,
+    });
+    const data = await res.text();
+    if (data === "false") {
+      throw new Error("false from api");
+    }
+    return await sign({ email }) as string;
+  },
 };
