586d664b3751b5aceb80f10d79586c31a7076bd3
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2022-11-23 12:20:41 -0800

    feat: change menu options

---
+++ pages/api/academic_unit/[id].ts
@@ -0,0 +1,33 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+
+interface ErrorResponse {
+    error: string;
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any[] | ErrorResponse>
+) {
+    try {
+        switch (req.method) {
+            case 'GET':
+                const response = await fetch(`${process.env.API}/escolar/catalogos/campus/${req.query.id}/unidades-academicas`, {
+                    headers: {
+                        'Authorization': process.env.API_AUTHORIZATION ?? ""
+                    }
+                }).then(response => response.json());
+                const academicUnits = response['unidadesAcademicas'].map((unidadAcademica: { idUnidadAcademica: string, nombreUnidadAcademica: string }) => (
+                    {
+                        id: unidadAcademica['idUnidadAcademica'],
+                        nombre: unidadAcademica['nombreUnidadAcademica']
+                    }
+                ));
+                const total = academicUnits.length;
+                return res.status(200).setHeader('Content-Range', `0-${total - 1}/${total}`).json(academicUnits);
+            default:
+                throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        return res.status(500).json({"error": (error as any).message});
+    }
+}
