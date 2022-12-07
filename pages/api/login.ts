372073d08e12cdae33d3a8a7043e38af7719d8df
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2022-12-07 12:49:24 -0800

    fix: desactive login with permissions

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -19,13 +19,10 @@ export default async function handler(
         switch (req.method) {
             case 'POST':
                 const authenticated = await isAuthenticated(req.body.username, req.body.password);
-                const user = await userController.getUser(req.body.username);
-                const authorized = authenticated && user.can('login');
-                if (!authorized) {
+                if (!authenticated) {
                     return res.status(401).json({ message: "ra.auth.sign_in_error" });
                 }
-                const permissions = user.getPermissions();
-                const token = await sign({ email: `${req.body.username}@uabc.edu.mx`, claims: permissions}) as string;
+                const token = await sign({ email: `${req.body.username}@uabc.edu.mx` }) as string;
                 return res.status(201).json({ token });
             default:
                 return res.status(406).json({message: "ra.notification.http_error"});
